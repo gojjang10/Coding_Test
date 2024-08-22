e92c0534f2eed67caab0577ac2bb8c4a0401167e
gojjang10 <gojjang2@gmail.com>
2024-08-23 01:24:25 +0900

    feat : 프로그래머스 120812번 문제 해결

--- CodingTest/problem_120812/Program.cs
+++ CodingTest/problem_120812/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace problem_120812
+﻿using System.ComponentModel.Design;
+
+namespace problem_120812
 {
     /// <summary>
     ///  2024년 8월 7일
@@ -15,12 +17,69 @@
             public int solution(int[] array)
             {
                 int answer = 0;
-                Array.Sort(array);
-                int sameNum = array[0];
+                // 반환 값
+                int maxValue = 0;
+                // 배열의 최대 수
 
-                for(int i = 0; i < array.Length; i++)
+                foreach (int value in array)
+                    // 입력받은 배열 반복
                 {
+                    if(value > maxValue)
+                        // 최대 수보다 현재 배열 속 값이 크다면
+                    {
+                        maxValue = value;
+                        // 최대값을 현재 배열 속 값으로 초기화
+                    }
+                }
 
+                int[] countArray = new int[maxValue + 1];
+                // 카운트를 세는 배열 선언. 그리고 배열의 인덱스는 0부터 시작하기 때문에 + 1 추가
+
+                foreach (int value in array)
+                    // 입력받은 배열 다시 반복
+                {
+                    countArray[value]++;
+                    // 입력받은 배열 숫자를 카운트를 새는 배열에 넣으면 입력받은 배열 속 숫자의 값의 카운트를 셀 수 있다.
+                    // 중복되면 해당 인덱스의 값을 1씩 추가한다
+                }
+
+                int mode = 0;
+                // 최빈값 변수
+                int modeCount = 0;
+                // 최빈값의 카운트
+                bool sameValue = false;
+                // 최빈값의 중복 여부
+
+                for (int i = 0; i <= maxValue; i++)
+                {
+                    if (countArray[i] > modeCount)
+                        // 카운트 배열 해당 숫자 안에 카운트가 최빈값 카운트보다 크다면
+                    {
+                        modeCount = countArray[i];
+                        // 최빈값 카운트를 초기화
+                        mode = i;
+                        // 최빈값 현재 i로 초기화
+                        sameValue = false ;
+                        // 중복 여부 거짓
+                    }
+                    else if(countArray[i] == modeCount)
+                        // 카운트 배열 해당 숫자 안에 카운트가 최빈값 카운트와 같다면
+                    {
+                        sameValue = true ;
+                        // 중복 여부 참
+                    }
+                }
+               
+                if (sameValue)
+                    // 중복일때
+                {
+                    answer = -1;
+                    // -1 저장
+                }
+                else
+                {
+                    answer = mode;
+                    // 중복이 아니면 최빈값 저장
                 }
 
                 return answer;
