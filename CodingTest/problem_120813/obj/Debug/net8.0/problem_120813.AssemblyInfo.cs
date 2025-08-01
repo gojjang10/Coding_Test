7b60974ec25a35b07b8641c5a2701a17827df431
gojjang10 <gojjang2@gmail.com>
2025-08-01 17:04:08 +0900

    feat : 프로그래머스 120818번 문제 해결

--- CodingTest/problem_120813/obj/Debug/net8.0/problem_120813.AssemblyInfo.cs
+++ CodingTest/problem_120813/obj/Debug/net8.0/problem_120813.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("problem_120813")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08a4549f25a6de4941cb6417f9d19648375a16c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+633b60392c97bde7d9b26b0a20d855155c31f0bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("problem_120813")]
 [assembly: System.Reflection.AssemblyTitleAttribute("problem_120813")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ CodingTest/problem_120818/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace problem_120818
+{
+    /// <summary>
+    ///  2025년 8월 1일
+    ///  문제 : 머쓱이네 옷가게는 10만 원 이상 사면 5%, 30만 원 이상 사면 10%, 50만 원 이상 사면 20%를 할인해줍니다.
+    ///  구매한 옷의 가격 price가 주어질 때, 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
+    ///  URL : https://school.programmers.co.kr/learn/courses/30/lessons/120818
+    /// </summary>
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int price)
+        {
+            double answer = 0;
+
+            if(price >= 500000)
+            {
+                answer = price * 0.8;
+            }
+            else if(price >= 300000)
+            {
+                answer = price * 0.9;
+            }
+            else if( price >= 100000)
+            {
+                answer = price * 0.95;
+            }
+            else
+            {
+                answer = price;
+            }
+
+            return (int)answer;
+        }
+    }
+}
