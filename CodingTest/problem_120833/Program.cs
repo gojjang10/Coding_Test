1c6175b9eadfa6f46741da6412c0fa5881d594db
gojjang10 <gojjang2@gmail.com>
2025-08-10 23:38:52 +0900

    feat : 프로그래머스 120833번 문제 추가

---
+++ CodingTest/problem_120833/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace problem_120833
+{
+
+    /// <summary>
+    ///  2025년 8월 9일
+    ///  문제 : 정수 배열 numbers와 정수 num1, num2가 매개변수로 주어질 때, 
+    ///  numbers의 num1번 째 인덱스부터 num2번째 인덱스까지 자른 정수 배열을 return 하도록 solution 함수를 완성해보세요.
+    ///  URL : https://school.programmers.co.kr/learn/courses/30/lessons/120833
+    /// </summary>
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Solution
+    {
+        public int[] solution(int[] numbers, int num1, int num2)
+        {
+            int[] answer = new int[] { };
+            return answer;
+        }
+    }
+}
