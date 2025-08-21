cc8d78f954f245de58bf001ef9e0d633a2c928ac
gojjang10 <gojjang2@gmail.com>
2025-08-21 15:02:34 +0900

    feat : 프로그래머스 120836번 문제 해결

---
+++ CodingTest/problem_120836/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace problem_120836
+{
+
+    /// <summary>
+    ///  2025년 8월 21일
+    ///  문제 : 순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a, b)로 표기합니다.
+    ///  자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를 return하도록 solution 함수를 완성해주세요.
+    ///  URL : https://school.programmers.co.kr/learn/courses/30/lessons/120836
+    /// </summary>
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if(n % i == 0)
+                {
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
