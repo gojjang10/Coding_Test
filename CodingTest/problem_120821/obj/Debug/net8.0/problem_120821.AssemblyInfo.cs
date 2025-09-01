706093d631ca1a99cf2b7dbf53c0a98d11e8ab27
gojjang10 <gojjang2@gmail.com>
2025-09-01 21:27:51 +0900

    feat : 프로그래머스 120846번 문제 해결

--- CodingTest/problem_120821/obj/Debug/net8.0/problem_120821.AssemblyInfo.cs
+++ CodingTest/problem_120821/obj/Debug/net8.0/problem_120821.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("problem_120821")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eda97ad942c922f21d7de1da7eb5c033e696b7dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d7d96a2e8301974338fc3eea18d05ae1164ed95")]
 [assembly: System.Reflection.AssemblyProductAttribute("problem_120821")]
 [assembly: System.Reflection.AssemblyTitleAttribute("problem_120821")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ CodingTest/problem_120846/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace problem_120846
+{
+
+    /// <summary>
+    ///  2025년 9월 1일
+    ///  문제 : 약수의 개수가 세 개 이상인 수를 합성수라고 합니다. 
+    ///  자연수 n이 매개변수로 주어질 때 n이하의 합성수의 개수를 return하도록 solution 함수를 완성해주세요.
+    ///  URL : https://school.programmers.co.kr/learn/courses/30/lessons/120846
+    /// </summary>
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if(Result(i) == true)
+                {
+                    answer++;
+                }
+            }
+
+            return answer;
+        }
+
+        public bool Result(int n)
+        {
+            bool result = false;
+            int count = 0;  
+
+            for (int i = 1; i <= n; i++)
+            {
+                if(n % i == 0)
+                {
+                    count++;
+                }
+            }
+
+            if(count >= 3)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
