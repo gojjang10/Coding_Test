fdf28d1e778b295274d7639afd92847eacfa8640
gojjang10 <gojjang2@gmail.com>
2025-09-04 20:13:08 +0900

    feat : 프로그래머스 120848번 문제 해결

--- CodingTest/problem_120808/obj/Debug/net8.0/problem_120808.AssemblyInfo.cs
+++ CodingTest/problem_120808/obj/Debug/net8.0/problem_120808.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("problem_120808")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+706093d631ca1a99cf2b7dbf53c0a98d11e8ab27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1286a7b1df93a56d946588af2a16d502b2415d7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("problem_120808")]
 [assembly: System.Reflection.AssemblyTitleAttribute("problem_120808")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ CodingTest/problem_120848/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace problem_120848
+{
+
+    /// <summary>
+    ///  2025년 9월 4일
+    ///  문제 : i팩토리얼 (i!)은 1부터 i까지 정수의 곱을 의미합니다. 
+    ///  예를들어 5! = 5 * 4 * 3 * 2 * 1 = 120 입니다. 정수 n이 주어질 때 다음 조건을 만족하는 가장 큰 정수 i를 return 하도록 solution 함수를 완성해주세요.
+    ///  URL : https://school.programmers.co.kr/learn/courses/30/lessons/120848
+    /// </summary>
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            for (int i = 1; ; i++)
+            {
+                if (Factorial(i) == n)
+                {
+                    return i;
+                }
+                else if (Factorial(i) >= n)
+                {
+                    return i - 1;
+                }
+
+            }
+        }
+
+        public int Factorial(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return n * Factorial(n - 1);
+        }
+
+    }
+}
