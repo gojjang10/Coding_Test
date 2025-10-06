f95d0b7ecbab96046e8e2a0b6529595e0bbed507
gojjang10 <gojjang2@gmail.com>
2025-10-06 16:29:02 +0900

    feat : 프로그래머스 120583번 문제 해결

---
+++ CodingTest/problem_120583/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace problem_120583
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int[] array, int n)
+        {
+            int count = 0;
+
+            foreach (int num in array)
+            {
+                if ( num == n ) count++;
+            }
+            return count;
+        }
+    }
+}
--- CodingTest/problem_120805/obj/Debug/net8.0/problem_120805.AssemblyInfo.cs
+++ CodingTest/problem_120805/obj/Debug/net8.0/problem_120805.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("problem_120805")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06b3eabfa292ea2cd856546d2319df04e0947871")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f727eb9da3763aed89d105d63568ffab8e9a80")]
 [assembly: System.Reflection.AssemblyProductAttribute("problem_120805")]
 [assembly: System.Reflection.AssemblyTitleAttribute("problem_120805")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
