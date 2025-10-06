c8eda69b2be731dc7d6dcc70ade78a40c28c01e5
gojjang10 <gojjang2@gmail.com>
2025-10-06 16:39:17 +0900

    feat : 프로그래머스 120585번 문제 해결

--- CodingTest/problem_120583/Program.cs
+++ CodingTest/problem_120583/Program.cs
@@ -1,5 +1,11 @@
 ﻿namespace problem_120583
 {
+
+    /// <summary>
+    ///  2025년 10월 6일
+    ///  문제 : 정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때, array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
+    ///  URL : https://school.programmers.co.kr/learn/courses/30/lessons/120583
+
     internal class Program
     {
         static void Main(string[] args)
--- CodingTest/problem_120905/obj/Debug/net8.0/problem_120905.AssemblyInfo.cs
+++ CodingTest/problem_120905/obj/Debug/net8.0/problem_120905.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("problem_120905")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f727eb9da3763aed89d105d63568ffab8e9a80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f95d0b7ecbab96046e8e2a0b6529595e0bbed507")]
 [assembly: System.Reflection.AssemblyProductAttribute("problem_120905")]
 [assembly: System.Reflection.AssemblyTitleAttribute("problem_120905")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
