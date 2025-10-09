3bfa1ce32f617fae44cd325e64263ca44485120f
gojjang10 <gojjang2@gmail.com>
2025-10-09 23:54:56 +0900

    feat : 프로그래머스 120860번 문제 추가

--- CodingTest/problem_120585/obj/Debug/net8.0/problem_120585.AssemblyInfo.cs
+++ CodingTest/problem_120585/obj/Debug/net8.0/problem_120585.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("problem_120585")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f95d0b7ecbab96046e8e2a0b6529595e0bbed507")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8eda69b2be731dc7d6dcc70ade78a40c28c01e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("problem_120585")]
 [assembly: System.Reflection.AssemblyTitleAttribute("problem_120585")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
